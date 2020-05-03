7fd6fafba0c135621eba634c70f9491a4a560c79
Jan-Willem Steeb <jsteeb@nrao.edu>
2020-05-03 18:18:22 -0400

    Update setup.py
    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', "r") as fid:   #encoding='utf-8'
 
 setup(
     name='ngcasa',
-    version='0.0.3',
+    version='0.0.4',
     description='Next Generation CASA',
     long_description=long_description,
     long_description_content_type="text/markdown",
