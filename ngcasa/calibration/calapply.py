741db5e3e37ae9feea17df0238dfcf4584f94105
Ryan Raba <ryanraba@gmail.com>
2020-05-17 17:56:19 -0400

    calibration documentation updates

--- ngcasa/calibration/calapply.py
+++ ngcasa/calibration/calapply.py
@@ -14,23 +14,25 @@
 
 def calapply(vis_dataset, cal_dataset, apply_parms, storage_parms):
     """
+    .. todo::
+        This function is not yet implemented
+    
     Apply antenna gain solutions according to the parameters in solpars.
     
     Calculate  V_ij(corrected) = V_ij(observed) / g_i g_j*
     
-    
     Inputs :
-        List of calibration solution datasets (to apply in the specified order)
-        Interpolation type ?
-        Data array on which to operate. Default='DATA'
-        Data array in which to fill the output.  Default='CORRECTED_DATA'
-                - this option exists in order to support simulator's corrupt operation where we'd pick 'DATA'
     
+    List of calibration solution datasets (to apply in the specified order)
+    Interpolation type ?
+    Data array on which to operate. Default='DATA'
+    Data array in which to fill the output.  Default='CORRECTED_DATA'
+    - this option exists in order to support simulator's corrupt operation where we'd pick 'DATA'
     
     TBD : Should this translation of the caltable back to the original un-averaged data be done here,
-          or in a centralized CNGI method that handles such interpolations for all methods that need
-          to convert averaged values into un-averaged values.  Note the difference between just copying
-          and expanding values, versus interpolation.
+    or in a centralized CNGI method that handles such interpolations for all methods that need
+    to convert averaged values into un-averaged values.  Note the difference between just copying
+    and expanding values, versus interpolation.
           
     TBD : Single apply() method, or several ?
     
--- ngcasa/calibration/calsolve.py
+++ ngcasa/calibration/calsolve.py
@@ -1,4 +1,4 @@
-#   Copyright 2019 AUI, Inc. Washington DC, USA
+#   Copyright 2020 AUI, Inc. Washington DC, USA
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -15,18 +15,25 @@
 
 def calsolve(vis_dataset, cal_dataset, solve_parms, storage_parm):
     """
+    .. todo::
+        This function is not yet implemented
+    
     Calculate antenna gain solutions according to the parameters in solpars.
     The input dataset has been pre-averaged/processed and the model visibilities exist
     
     Iteratively solve the system of equations g_i g_j* = V_data_ij/V_model_ij  for all ij.
     Construct a separate solution for each timestep and channel in the input dataset.
     
-    Options : amp, phase or both
-              solution type (?) G-term, D-term, etc...
-              Data array for which to calculate solutions. Default='DATA'
-              
-    TBD : Single method with options for solutions of different types ?
-          Or, separate methods for G/B, D, P etc.. : solve_B, solve_D, solve_B, etc...
+    Options :
+    
+    amp, phase or both
+    solution type (?) G-term, D-term, etc...
+    Data array for which to calculate solutions. Default='DATA'
+    
+    TBD :
+    
+    Single method with options for solutions of different types ?
+    Or, separate methods for G/B, D, P etc.. : solve_B, solve_D, solve_B, etc...
           
     Returns
     -------
