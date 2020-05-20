1914b71f4d7f553820306ffe79887d6cc936cc2f
Ryan Raba <ryanraba@gmail.com>
2020-05-20 15:12:19 -0400

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', "r") as fid:   #encoding='utf-8'
 
 setup(
     name='ngcasa',
-    version='0.0.9',
+    version='0.0.10',
     description='Next Generation CASA',
     long_description=long_description,
     long_description_content_type="text/markdown",
